23140b494ef6edf5e7f52d2220270da2716e7ae6
rigobersio <rigomartinez@udec.cl>
2023-03-09 18:57:22 -0300

    edicion de eliminar.js y creacion de eliminar3.js

--- eliminar.js
+++ eliminar.js
@@ -4,7 +4,15 @@ texto = `
 2. esto otro lo estoy escribiendo en main2
 3. creada en main2 luego de merge exitoso para comprobar actualizacion github (con o sin push?)
 4. aparentemento main1 y 2 están fusionadas.se creara un commnit
-5. se elimino y creo nuevamente main2. se creo esta linea + un nuevo archivo.
+5. se elimino main2 y creo nuevamente. se creo esta linea + un nuevo archivo.
+
+6. se edito la linea 5  se descubrio que aun despues de hacer
+ git add no hay diferencia entre las ramas
+nuevamente se elimino y se volvio a crear, inmediatamente
+ despues se hizo un commit sin ningun cambio.
+7. ahora si hay diferencia: no se puede cambiar de rama sin guardar cambios
+en git (ni siquiera admite git add tiene que ser commit
+8. aparte de editar este archivo se creara otro archivo en blanco
 
 
 
