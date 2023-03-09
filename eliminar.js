3164f76ba7e5fff6bcd29f722bc75ccfcdbbd803
rigobersio <rigomartinez@udec.cl>
2023-03-09 18:28:07 -0300

    (win)___eliminar.js(aparenetemente hay fusion main1 y 2)

--- eliminar.js
+++ eliminar.js
@@ -1,2 +1,10 @@
-esto lo estoy escribiendo en la rama main
-esto otro lo estoy escribiendo en main2
+var texto;
+texto = `
+1. esto lo estoy escribiendo en la rama main
+2. esto otro lo estoy escribiendo en main2
+3. creada en main2 luego de merge exitoso para comprobar actualizacion github (con o sin push?)
+4. aparentemento main1 y 2 están fusionadas. se creara un commnit
+
+`
+
+console.log(texto);
