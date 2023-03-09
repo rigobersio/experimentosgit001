8b8d1db1d2105438bfe8a64cbc750c3b695d267f
rigobersio <rigomartinez@udec.cl>
2023-03-09 18:49:22 -0300

    primer commit en main2: ningun cambio

--- eliminar.js
+++ eliminar.js
@@ -3,7 +3,12 @@ texto = `
 1. esto lo estoy escribiendo en la rama main
 2. esto otro lo estoy escribiendo en main2
 3. creada en main2 luego de merge exitoso para comprobar actualizacion github (con o sin push?)
-4. aparentemento main1 y 2 están fusionadas. se creara un commnit
+4. aparentemento main1 y 2 están fusionadas.se creara un commnit
+5. se elimino y creo nuevamente main2. se creo esta linea + un nuevo archivo.
+
+
+
+
 
 `
 
